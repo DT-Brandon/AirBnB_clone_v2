7a81ea0f030a825c9b10096ea4a85e2d65a81796
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-04-21 06:06:30 -0700

    task sql test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a user and grant privileges
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all permi
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privileges
