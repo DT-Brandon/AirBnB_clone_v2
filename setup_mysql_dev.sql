99141685f7b4ebdb58e63fb090d4251815d25260
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-04-21 05:04:12 -0700

    sql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,5 +6,5 @@ GRANT ALL ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant SELECT privileges on performance_schema
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost'; 
